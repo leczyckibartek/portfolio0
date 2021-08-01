b794a94f7783c7e3475fd83207c7d3aa99cbc299
Bartosz Łęczycki <leczyckibartek@gmail.com>
2021-08-01 16:31:17 +0200

    parallax

--- src/components/Content/ACF/Custom/HeroDetail/index.js
+++ src/components/Content/ACF/Custom/HeroDetail/index.js
@@ -3,6 +3,7 @@ import './style.scss'
 import Intro from '../../../../../animations/Intro'
 import { GatsbyImage } from 'gatsby-plugin-image'
 import { useInView } from 'react-intersection-observer'
+import { Parallax } from 'react-scroll-parallax';
 
 function CustomBlock({images: img, files: file, fields: field, wysiwygs: wysiwyg, maps: map, delay: del}) {
 
@@ -12,12 +13,14 @@ function CustomBlock({images: img, files: file, fields: field, wysiwygs: wysiwyg
 		<>
 	    <div ref={io}>
         {/* <Intro visible={ioInView} in={{ fadeBottom: 1000 }} delayIn={125} mounted={true} stay={true} className={``}> */}
+        <Parallax className="custom-class" y={[50, -20]} tagOuter="figure">
           <Intro visible={ioInView} in={{ slideBottom: 1000 }} delayIn={125} mounted={true} stay={true} className={``}>
             <h1 className={`font1 wysiwyg-inject`} dangerouslySetInnerHTML={{__html: field[0]}} />
           </Intro>
           <Intro visible={ioInView} in={{ slideBottom: 1000 }} delayIn={125} mounted={true} stay={true} className={``}>
             <h1 className={`wysiwyg-inject`} dangerouslySetInnerHTML={{__html: field[1]}} />
           </Intro>
+          </Parallax>
         {/* </Intro> */}
       </div> 
 		</>
